6283c6fc4445b5bb532323940c0580cf95ca0b46
RodrigoVitor <rodrigovitorsc2001@gmail.com>
2021-01-17 17:09:30 -0300

    arquivo js

---
+++ public/js/verifAprende.js
@@ -0,0 +1,7 @@
+
+function showAsk(){
+    var doCurso = document.getElementById("doCurso")
+    var ask = document.getElementById("ask")
+    doCurso.style.visibility = "hidden"
+    ask.style.visibility = "visible"
+}
